51fb2b94f527be28ffa84afb4f88fc708f1e08c7
김용범 <subum12345@gmail.com>
2023-04-04 23:42:13 +0900

    string ts

---
+++ typescript_practice2/string.js
@@ -0,0 +1,7 @@
+"use strict";
+let fullName = "김용범";
+let age = 22;
+let sentence = `my name is ${fullName}. 
+
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
---
+++ typescript_practice2/string.ts
@@ -0,0 +1,8 @@
+let fullName: string = "김용범"
+let age: number = 22
+
+let sentence: string = `my name is ${fullName}. 
+
+I'll be ${age + 1} years old next month.`
+
+console.log(sentence);
