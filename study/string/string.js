3a791884b7c4640afbc75f7914d39cae72caca17
김용범 <subum12345@gmail.com>
2023-09-11 22:36:59 +0900

    study

---
+++ study/string/string.js
@@ -0,0 +1,4 @@
+var fullName = 'kim';
+var age = 28;
+var sentence = "my name is ".concat(fullName, " and age is ").concat(age);
+console.log(sentence);
---
+++ study/string/string.ts
@@ -0,0 +1,7 @@
+let fullName: string = 'kim';
+let age: number = 28;
+
+// es6 부터 Template String 가능 - 백틱으로 스트링 쓰기
+let sentence: string = `my name is ${fullName} and age is ${age}`;
+
+console.log(sentence);
