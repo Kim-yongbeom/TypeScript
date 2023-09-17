9547ddaf6488344f7bf9d5e0c95d6667f2c5450b
Kim <subum12345@gmail.com>
2023-09-17 18:40:49 +0900

    study

--- study/symbol/symbol.js
+++ study/symbol/symbol.js
@@ -1,1 +1,13 @@
+var _a;
 console.log(Symbol('foo') === Symbol("foo"));
+var sym = Symbol();
+// obj 객체의 sym에 자유롭게 접근가능한 코드
+// const obj = {
+//     sym: "value",
+// }
+// obj["sym"]
+// symbol 타입을 사용해서 Symbol로 정의한 값이 들어와야만 결과 보여줌
+var obj = (_a = {},
+    _a[sym] = "value",
+    _a);
+obj[sym];
--- study/symbol/symbol.ts
+++ study/symbol/symbol.ts
@@ -1,1 +1,18 @@
 console.log(Symbol('foo') === Symbol("foo"));
+
+const sym = Symbol();
+
+// obj 객체의 sym에 자유롭게 접근가능한 코드
+// const obj = {
+//     sym: "value",
+// }
+
+// obj["sym"]
+
+
+// symbol 타입을 사용해서 Symbol로 정의한 값이 들어와야만 결과 보여줌
+const obj = {
+    [sym]: "value",
+}
+
+obj[sym]
